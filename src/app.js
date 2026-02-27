68fcd135560a1a69e6e3ab1548dec70fd6ea1337
Elizabeth Reynolds <e.d.reynolds@icloud.com>
2026-02-27 17:54:03 -0600

    js logic

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+ 
+  // build card generator
+
+// step 1. build values & suit arrays
+
+  const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  const suits = ['♠', '♥', '♣', '♦']; //alt codes
+
+  let randomValue = Math.floor(Math.random() * values.length);
+
+  let randomSuit = Math.floor(Math.random() * suits.length)
+
+  console.log(suits[randomSuit], values[randomValue], suits[randomSuit]);
+
+
+
 };
